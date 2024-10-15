04c6b163b98b757ecd8fab2c95555c2f1cc9afd4
NIGHT <3210103847@zju.edu.cn>
2024-10-15 15:47:16 +0800

    modified

--- server/servertest.py
+++ server/servertest.py
@@ -63,43 +63,6 @@ if __name__ == '__main__':
 
 
 
-# import requests
-# import sqlite3  
-# url = "http://localhost:11434/api/generate"  # 更新为生成请求的API端点
-# headers = {
-#     "Content-Type": "application/json"
-# }
-# promptai = "——————————将以上文本提取关键词，针对他讨论的话题划分为相对应的类别，回答请使用json格式"
-# data = {
-#     "model": "llama3.2",  # 指定模型名称
-#     "prompt": update_data+promptai,  # 输入文本 + prompt
-#     "stream": False  # 设置为false以获取单个响应
-# }
-# response = requests.post(url, json=data, headers=headers)
-
-# if response.status_code == 200:
-#     print("响应内容：", response.json())
-#     conn = sqlite3.connect('user_data.db')
-#     cursor = conn.cursor()
-#     cursor.execute('''CREATE TABLE IF NOT EXISTS user_data (
-#         user_id TEXT,
-#         secret_key TEXT,
-#         data TEXT,
-#         time TEXT,
-#         category TEXT
-#     )''')
-#     # 修复：将 "kaj123" 替换为变量 secret_key，并添加 user_id 和其他字段
-#     cursor.execute('''INSERT INTO user_data (user_id, secret_key, data) VALUES (?, ?, ?)''', (user_id, secret_key, response.json()))  # 使用变量 user_id 和 secret_key
-#     conn.commit()
-#     conn.close()
-
-# else:
-#     print("请求失败，状态码：", response.status_code)
-
-
-#得到数据response 加密
-
-# 添加到数据库里面
 
 
 
